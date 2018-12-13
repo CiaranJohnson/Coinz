a59afc38683c49fb0d402e0b57930d67faeca950
CiaranJohnson <ciaranmjohnson@aol.com>
2018-12-13 09:42:14 +0000

    app icon changed

--- app/src/androidTest/java/com/example/tech/coinz/MapButtonTest.java
+++ app/src/androidTest/java/com/example/tech/coinz/MapButtonTest.java
@@ -20,6 +20,10 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 
+/**
+ * These test to see if all the map buttons in different activities work as expected.
+ */
+
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class MapButtonTest {
